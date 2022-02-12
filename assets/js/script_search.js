e81980d6b83e3de684824ba6d8140d601c134545
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-02-11 20:48:57 -0600

    minor adjustments

--- assets/js/script_search.js
+++ assets/js/script_search.js
@@ -2,4 +2,5 @@ function search() {
             //   var dd = String(this.date.getDate()).padStart(2, '');
             //   var mm = String(this.date.getMonth() + 1).padStart(2, '');
             //   var yyyy = this.date.getFullYear();
-              console.log(searchdd,searchmm,searchyyyy) }
+            //   console.log(searchdd,searchmm,searchyyyy) 
+}
